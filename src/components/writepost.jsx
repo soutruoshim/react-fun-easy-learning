bf46451742b0697fda99984b6cecde33c3267c03
sout_rahim <sout.rahim@gmail.com>
2022-04-18 22:38:11 +0700

    http post data

--- src/components/writepost.jsx
+++ src/components/writepost.jsx
@@ -1,9 +1,45 @@
 import React, { Component } from "react";
+import axios from "axios";
 export default class WritePost extends Component{
+    state= {
+        title:"",
+        body:""
+    }
+    formSubmit = (e) =>{
+        e.preventDefault(); // this page not load
+        axios.post('https://jsonplaceholder.typicode.com/posts', {
+            title: this.state.title,
+            body: this.state.body
+          })
+          .then(function (response) {
+              console.log('success')
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+    }
     render(){
         return(
             <div>
-                <h1>This is write Post</h1>
+                <div class="container-sm">
+                <div class="card">
+                    <div class="card-body">
+                        <form onSubmit={this.formSubmit}>
+                            <div class="mb-3">
+                                <label class="form-label">Title</label>
+                                <input type="text" class="form-control" id="title" name="title" onChange={e=>{this.setState({title:e.target.value})}} />
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label">Body</label>
+                                <textarea class="form-control" id="body" name="body" onChange={e=>{this.setState({body:e.target.value})}}></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+                </div>
             </div>
         )
     }
